9e9639cd1410395acdd502d85bd9e9341e4b7d7d
MJMrigg <riggmatthew20@outlook.com>
2025-10-30 22:24:35 -0400

    Dialog Box shows
    Work Done:
    - Programmer can set dialog options and answers from inspector.
    - Dialog box shows when player initiates dialog.
    
    Todo:
    - Add grid to dialog box and have interactable object fill it with dialog options.
    - Allow player to select options and change focus between options.
    - Update dialog box based off of selected dialog options.

--- new-game-project/Scripts/Interactable.cs
+++ new-game-project/Scripts/Interactable.cs
@@ -1,7 +1,22 @@
 using Godot;
 using System;
 
-public partial class Interactable : CharacterBody2D
-{
+public partial class Interactable : CharacterBody2D{
 	[Export] public Control E;
+	[Export] public Control DialogScreen;
+	[Export] public String[] DialogOptions;
+	[Export] public String[] DialogAnswers;
+	
+	public void Interact(){
+		//If there is no dialog screen, return
+		if(DialogScreen == null){
+			GD.Print("Error: No Dialog Screen");
+			return;
+		}
+		//Show the dialog screen
+		((ColorRect)DialogScreen.GetNode("Screen")).Show();
+		for(int i = 0; i < DialogOptions.Length; i++){
+			GD.Print(DialogOptions[i]);
+		}
+	}
 }
--- new-game-project/Scripts/Player.cs
+++ new-game-project/Scripts/Player.cs
@@ -3,29 +3,61 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
+	public float Speed = 300.0f; //Movement speed
+	public Interactable interacted = null; //Object to be interacted with
+	public bool IsInteracting = false; //Whether the player is interacting
 
 	public override void _Process(double delta){
 		//Get Inputs
-		Vector2 translate = Input.GetVector("p_left","p_right","p_up","p_down");
+		float translate_x = Input.GetAxis("p_left","p_right");
+		float translate_y = Input.GetAxis("p_up","p_down");
 		//Set up velocity
-		Vector2 DesiredVelocity = translate*Speed;
-		Velocity = DesiredVelocity;
-		//FREWAKING MOVEAS!!! SERIOUSLY, STOP FOGETTING THIS STESP!@
-		MoveAndSlide();
+		Vector2 DesiredVelocity = Vector2.Zero;
+		//Get direction of velocity
+		if(Math.Abs(translate_x) >= Math.Abs(translate_y)){
+			DesiredVelocity = new Vector2(translate_x,0);
+		}else{
+			DesiredVelocity = new Vector2(0,translate_y);
+		}
+		//Add speed
+		Velocity = DesiredVelocity*Speed;
+		//FREWAKING MOVEAS!!! SERIOUSLY, STOP FOGETTING THIS STESP!
+		if(!IsInteracting){ //Unless the player is interacting with something
+			MoveAndSlide();
+		}
+		
+		//If the player presses E, interact
+		if(Input.IsActionJustPressed("p_interact")){
+			Interact();
+		}
 	}
 	
+	//Show an object is interactable
 	public void ShowInteractable(Node2D Body){
 		if(Body is Interactable){
 			Interactable InteractionObject = (Interactable)Body;
 			InteractionObject.E.Visible = true;
+			interacted = (Interactable)Body;
 		}
 	}
 	
+	//Stop showing an object is interactable
 	public void HideInteractable(Node2D Body){
 		if(Body is Interactable){
 			Interactable InteractionObject = (Interactable)Body;
 			InteractionObject.E.Visible = false;
+			interacted = null;
 		}
+	}
+	
+	//When the player interacts with an object
+	public void Interact(){
+		//If there is no object to interact with, return
+		if(interacted == null){
+			return;
+		}
+		//Start the interaction
+		IsInteracting = true;
+		interacted.Interact();
 	}
 }
