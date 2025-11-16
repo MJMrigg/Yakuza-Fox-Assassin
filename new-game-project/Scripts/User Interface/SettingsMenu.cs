09a8b0ffbcd591c4285dde1cd316b75bc7270899
Morriban <gamerdave3@gmail.com>
2025-11-16 16:37:38 -0500

    Added volume adjustment to Settings
    Create 2 new bus's for the audio in settings. To get the audio to work, properly for each instance of played audio you will need to assign it to the appropriate bus.

--- new-game-project/Scripts/User Interface/SettingsMenu.cs
+++ new-game-project/Scripts/User Interface/SettingsMenu.cs
@@ -8,6 +8,9 @@ public partial class SettingsMenu : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		//GD.Print(InputMap.GetActions());
+		//InputMap.ActionAddEvent("p_melee", );
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,23 +24,32 @@ public partial class SettingsMenu : Control
 		
 	}
 	
+	//
+	public void buttonToggle(bool tog)
+	{
+		
+	}
+	
 	//Change the current master volume
 	public void ChangeMasterVolume(float NewVolume)
 	{
-		
+		//audiobus 0 is master bus
+		AudioServer.SetBusVolumeDb(0, NewVolume);
 	}
 	
 	
 	//Change the current sound effect volume
 	public void ChangeSoundVolume(float NewVolume)
 	{
-		
+		//audiobus 1 is the sound effect bus
+		AudioServer.SetBusVolumeDb(1, NewVolume);
 	}
 	
 	//Change the current music volume
 	public void ChangeMusicVolume(float NewVolume)
 	{
-		
+		//audiobus 2 is the music bus
+		AudioServer.SetBusVolumeDb(2, NewVolume);
 	}
 	
 	//Return to the scene
