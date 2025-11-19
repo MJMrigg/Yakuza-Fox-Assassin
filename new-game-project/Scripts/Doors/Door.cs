d16195328af085286905b0316a953329891c8c74
MJMrigg <riggmatthew20@outlook.com>
2025-11-18 22:38:32 -0500

    Door Functionality
    Work Done:
    - Doors can now detect the player and if they have the key or not.
    
    Next Priority:
    - Allow player to pickup the key and use it to unlock the door.

---
+++ new-game-project/Scripts/Doors/Door.cs
@@ -0,0 +1,89 @@
+using Godot;
+using System;
+
+public partial class Door : StaticBody2D
+{
+	public bool Unlocked; //If the door is unlocked or not
+	
+	[Export]
+	public bool DoorColor; //What color the door is
+	
+	[Export]
+	public int ConnectedRoom; //The room that the door leads to
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Unlocked = false; //Start the door with being unlocked
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	//Change if the door is locked or unlocked
+	public void ChangeLock(bool ConditionsMet)
+	{
+		if(ConditionsMet == DoorColor)
+		{
+			Unlocked = true;
+			ChangeRoom(Unlocked);
+		}
+	}
+	
+	//Check if the door is unlocked
+	public bool CheckLock()
+	{
+		return Unlocked;
+	}
+	
+	//Change to the next room
+	public void ChangeRoom(bool AllowedThrough)
+	{
+		GD.Print("TEST");
+	}
+	
+	//Detect if the player has the key or not
+	public void CheckPlayer(Node2D body)
+	{
+		//If the door is already unlocked, let the player through
+		if(CheckLock())
+		{
+			ChangeRoom(true);
+		}
+		//NPCs can't go through the door
+		if(!(body is Player))
+		{
+			return;
+		}
+		//Check the player's inventory for any keys
+		Player player = (Player)body;
+		Inventory PlayerInventory = player.Inv;
+		for(int i = 0; i < PlayerInventory.ItemsStored.Length; i++)
+		{
+			Item CurrentItem = PlayerInventory.ItemsStored[i];
+			//If the player does not have an item in that part of the inventory or that item is not a key, move on
+			if(CurrentItem == null || CurrentItem.ID < 4)
+			{
+				continue;
+			}
+			//Check if the key's color is the same as the door color
+			Key PotentialKey = (Key)CurrentItem;
+			ChangeLock(PotentialKey.KeyColor);
+			return;
+		}
+	}
+	
+	//Let the player through if they're coming in through the unlocked side
+	public void LetPlayerThrough(Node2D body)
+	{
+		//NPCs can't go through the door
+		if(!(body is Player))
+		{
+			return;
+		}
+		ChangeRoom(true);
+	}
+	
+}
